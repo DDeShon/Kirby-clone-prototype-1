4f59562bca5da0c5f612f2090fe1f454d4175a1d
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-05-12 23:41:43 -0500

    working on main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import { k } from "./kaboomCtx.ts";
+
+async function gameSetup() {
+    k.loadSprite("assets", "./kirby-like.png", {
+        sliceX: 9,
+        sliceY: 10,
+    })
+}
+
+gameSetup();
