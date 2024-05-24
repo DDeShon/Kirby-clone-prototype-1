b7e80fc383e7be87a26a8233638c5cb61b6a9a46
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-05-23 21:59:16 -0500

    working on utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -7,4 +7,16 @@ export async function makeMap(k: KaboomCtx, name: string) {
     const map = k.make([k.sprite(name), k.scale(scale), k.pos(0)]);
 
     const spawnPoints: {[key: string] : {x: number, y: number} } = {};
+
+    for (const layer of mapData.layers) {
+        if (layer.name === "colliders") {
+            for (const collider of layer.objects) {
+                map.add([
+                    k.area({
+                        shape: new k.Rect(k.vec2(0), collider.width, collider.height)
+                    })
+                ])
+            }
+        }
+    }
 }
