07df48e10694a0d4fb9f7b8faac36f0a5ae88566
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-05-23 21:56:12 -0500

    working on utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -5,4 +5,6 @@ export async function makeMap(k: KaboomCtx, name: string) {
     const mapData = await (await fetch(`./${name}.json`)).json();
 
     const map = k.make([k.sprite(name), k.scale(scale), k.pos(0)]);
+
+    const spawnPoints: {[key: string] : {x: number, y: number} } = {};
 }
