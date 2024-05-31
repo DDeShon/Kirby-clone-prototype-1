0e3ebb567fdccde727aac5a666a18fb5662d1203
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-05-30 23:18:42 -0500

    working on main.ts

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ async function gameSetup() {
         );
 
         k.add(kirb);
-        k.camScale(k.vec2(0.7));
+        k.camScale(0.7, 0.7);
         k.onUpdate(() => {
             if (kirb.pos.x < level1Layout.pos.x + 432)
                 k.camPos(kirb.pos.x + 500, 800);
